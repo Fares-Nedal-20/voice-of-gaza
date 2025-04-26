a2c0aac29885d65a23413f9030e01922ee8bb0a8
Fares-Nedal-20 <faresnedal770@gmail.com>
2025-04-26 13:41:07 +0300

    add some modifcations to OAuth

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -3,10 +3,22 @@ import React from "react";
 import { AiFillGoogleCircle } from "react-icons/ai";
 import { app } from "./../firebase";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  signInSuccess,
+  signInStart,
+  signInFailure,
+  signUpSuccess,
+} from "../redux/user/userSlice.js";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 export default function OAuth() {
+  const disptch = useDispatch();
+  const navigate = useNavigate();
+
   const handleGoogleClick = async () => {
     try {
+      disptch(signInStart);
       const auth = getAuth(app);
       const provider = new GoogleAuthProvider();
       provider.setCustomParameters({ prompt: "select_account" });
@@ -24,9 +36,16 @@ export default function OAuth() {
         }),
       });
       const data = await res.json();
-      console.log(data);
+      if (!res.ok) {
+        disptch(signInFailure(data.message));
+        return;
+      }
+      if (res.ok) {
+        disptch(signUpSuccess(data));
+        navigate("/");
+      }
     } catch (error) {
-      console.log(error);
+      disptch(signInFailure(error.message));
     }
   };
   return (
