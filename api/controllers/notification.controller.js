1ea571b26245c5f6f10a0b6b1b548f15e6dad01e
Fares-Nedal-20 <faresnedal770@gmail.com>
2025-07-26 15:17:43 +0300

    add mark as read and mark all as read api route

--- api/controllers/notification.controller.js
+++ api/controllers/notification.controller.js
@@ -9,10 +9,48 @@ export const getUserNotifications = async (req, res, next) => {
     );
   }
   try {
-    const notifications = await Notification.find({receiver: receiverId}).sort({
+    const notifications = await Notification.find({
+      receiver: receiverId,
+    }).sort({
       createdAt: -1,
     });
     res.status(200).json(notifications);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const markAsRead = async (req, res, next) => {
+  try {
+    const notification = await Notification.findById(req.params.notificationId);
+    if (!notification) {
+      return next(errorHandler(404, "Notification not found!"));
+    }
+    if (notification.receiver.toString() !== req.user.id) {
+      return next(
+        errorHandler(
+          401,
+          "You are not allowed to mark this notification as read!"
+        )
+      );
+    }
+    notification.isRead = true;
+    await notification.save();
+
+    res.status(200).json({ message: "Notification marked as read" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const markAllAsRead = async (req, res, next) => {
+  try {
+    await Notification.updateMany(
+      { receiver: req.user.id, isRead: false },
+      { $set: { isRead: true } }
+    );
+
+    res.status(200).json({ message: "All notifications marked as read" });
   } catch (error) {
     next(error);
   }
--- api/routes/notification.route.js
+++ api/routes/notification.route.js
@@ -1,9 +1,15 @@
 import express from "express";
 import { verifyToken } from "./../utils/verifyUser.js";
-import { getUserNotifications } from "../controllers/notification.controller.js";
+import {
+  getUserNotifications,
+  markAsRead,
+  markAllAsRead
+} from "../controllers/notification.controller.js";
 
 const router = express.Router();
 
 router.get("/getNotifications/:receiverId", verifyToken, getUserNotifications);
+router.put("/markAsRead/:notificationId", verifyToken, markAsRead);
+router.put("/markAllAsRead", verifyToken, markAllAsRead);
 
 export default router;
